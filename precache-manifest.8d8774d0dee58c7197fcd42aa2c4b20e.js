8ab0ac1214840fd5a499cec54438c2000c6d0134
Emmanuel <amdas72@gmail.com>
2020-03-25 13:42:18 +0200

    Updates

---
+++ precache-manifest.8d8774d0dee58c7197fcd42aa2c4b20e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdbcb42df97853a4ff55a9051004530",
+    "url": "/Robot-Friends/index.html"
+  },
+  {
+    "revision": "27d5138e769ee30802c2",
+    "url": "/Robot-Friends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "a80165547d9bfd299765",
+    "url": "/Robot-Friends/static/css/main.45db756e.chunk.css"
+  },
+  {
+    "revision": "27d5138e769ee30802c2",
+    "url": "/Robot-Friends/static/js/2.e636b603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robot-Friends/static/js/2.e636b603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a80165547d9bfd299765",
+    "url": "/Robot-Friends/static/js/main.0a3fc40e.chunk.js"
+  },
+  {
+    "revision": "1a03efec70b67b52c087",
+    "url": "/Robot-Friends/static/js/runtime-main.d050d38d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robot-Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd83255aa62c1b51bca36e6c6bec1052.js"
+  "/Robot-Friends/precache-manifest.8d8774d0dee58c7197fcd42aa2c4b20e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Robot-Friends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
