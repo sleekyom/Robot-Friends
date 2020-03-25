4dfe0d141c2753ed7bfd5ea083502e1c7229693f
Emmanuel <amdas72@gmail.com>
2020-03-25 06:25:51 +0200

    Updates

---
+++ precache-manifest.cd83255aa62c1b51bca36e6c6bec1052.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f81fb7d5d4293e7f2aa0d3c7fe9f3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27d5138e769ee30802c2",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "ca582d72bba7706a01bd",
+    "url": "/static/css/main.c040e61b.chunk.css"
+  },
+  {
+    "revision": "27d5138e769ee30802c2",
+    "url": "/static/js/2.e636b603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e636b603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca582d72bba7706a01bd",
+    "url": "/static/js/main.b6e1cc28.chunk.js"
+  },
+  {
+    "revision": "3a172e9daf29f6cc3071",
+    "url": "/static/js/runtime-main.08f98dcf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
